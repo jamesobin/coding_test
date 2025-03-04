3b5c0f030cc5de6641c0d3caec49c1c6af779c6d
jamesobin <jamesobin@naver.com>
2025-03-04 12:21:47 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.05 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.py
@@ -0,0 +1,8 @@
+def solution(my_string):
+    answer = 0
+    
+    for i in range(0, len(my_string)):
+        if my_string[i].isnumeric():
+            answer += int(my_string[i])
+            
+    return answer
