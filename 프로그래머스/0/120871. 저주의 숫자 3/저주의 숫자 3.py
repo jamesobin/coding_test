804395f976ab87e22160a0c7429ea7bcd9215b3e
jamesobin <jamesobin@naver.com>
2025-03-05 16:18:06 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
+++ 프로그래머스/0/120871. 저주의 숫자 3/저주의 숫자 3.py
@@ -1,9 +1,9 @@
 def solution(n):
-    num = []
-    for i in range(1, 200):
-        if i % 3 != 0 and i % 10 != 3 and i // 10 != 3 and i % 100 // 10 != 3:
-            num.append(i)
+    answer = 0
     
-    answer = num[n-1]
+    for i in range(0, n):
+        while answer % 3 == 0 or "3" in str(answer):
+            answer += 1
+        answer += 1
 
-    return answer
+    return answer - 1
