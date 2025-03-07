8f96e66a33e2ce7957d2b191ab140a4a66358eb4
jamesobin <jamesobin@naver.com>
2025-03-07 17:12:35 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 182.39 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.py
@@ -1,8 +1,11 @@
 def solution(elements):
     answer = []
-    circle = elements*2
-    for i,num in enumerate(elements):
+    circle = elements * 2
+    
+    for i in range(0, len(elements)):
+        num = elements[i]
         answer.append(num)
+        
         for num2 in circle[i+1:i+len(elements)]:
             num += num2
             answer.append(num)
